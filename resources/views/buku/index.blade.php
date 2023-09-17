c38b1b372336c3f3cbf3ec48aa0dbb45a501505c
ghitanajminaqasy <ghitanajminaqasy0304@mail.ugm.ac.id>
2023-09-17 13:30:17 +0700

    First Commit

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Judul Buku</th>
+                <th>Penulis</th>
+                <th>Harga</th>
+                <th>Tgl.Terbit</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data_buku as $buku)
+                <tr>
+                    <td>{{ $buku->id }}</td>
+                    <td>{{ $buku->judul }}</td>
+                    <td>{{ $buku->penulis }}</td>
+                    <td>{{ "Rp ".number_format($buku->harga, 2, ',', '.')}}</td>
+                    <td>{{ $buku->tgl_terbit->format('d/m/Y') }}</td>
+                </tr>
+            @endforeach
+        </tbody>        
+    </table>
+</body>
+</html>
